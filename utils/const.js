6cd9d30b7576cee1a0233bec368172214a142f35
lixiaoni <1165388551@qq.com>
2019-01-28 14:52:23 +0800

    1.28

--- utils/const.js
+++ utils/const.js
@@ -1,16 +1,16 @@
-// export const baseUrl = 'https://dev-www.youlife.me'
-// export const imageUrl = 'https://dev-image.youlife.me/'
-// export const uploadImg = 'https://dev-mall.youlife.me/base/image'
-// export const payUrl = "https://dev-pay.youlife.me/api/pay"
-// export const cloudUrl = "https://dev-www.youlife.me"
-// export const loginUrl = "https://dev-accounts.youlife.me"
+export const baseUrl = 'https://dev-www.youlife.me'
+export const imageUrl = 'https://dev-image.youlife.me/'
+export const uploadImg = 'https://dev-mall.youlife.me/base/image'
+export const payUrl = "https://dev-pay.youlife.me/api/pay"
+export const cloudUrl = "https://dev-www.youlife.me"
+export const loginUrl = "https://dev-accounts.youlife.me"
 
-export const baseUrl = 'https://mall.youlife.me/www'
-export const imageUrl = 'https://image.youlife.me/'
-export const payUrl = "https://pay.youlife.me/api/pay"
-export const uploadImg = 'https://mall.youlife.me/base/image'
-export const cloudUrl = "https://mall.youlife.me/www"
-export const loginUrl = "https://accounts.youlife.me"
+// export const baseUrl = 'https://mall.youlife.me/www'
+// export const imageUrl = 'https://image.youlife.me/'
+// export const payUrl = "https://pay.youlife.me/api/pay"
+// export const uploadImg = 'https://mall.youlife.me/base/image'
+// export const cloudUrl = "https://mall.youlife.me/www"
+// export const loginUrl = "https://accounts.youlife.me"
 export const basicAuthorization= 'Basic QmVpSmluZ0JhaVJvbmdTaGlNYW9DbGllbnQ6ZTU2YThmMWZkOWJlMmMzMzNmYjdiZTcyNjVkMjRhYTM=';
 export const owner = '10000'
 export const rStoreAppId = 'wx1a7532a2abdd1698'//零售店APPID
